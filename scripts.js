9ac89f9469f5f53f4418459a69e78ca3f8bc6367
erick-ondiwa <erickochieng3860@gmail.com>
2024-06-11 22:50:16 +0300

    Modfied

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
 const passwordLength = 12;
 const includeLowercase = true;
 const includeUpperCase = true;
+const includeNumbers = true;
 const includeSpecialChars = true;
 
+let allowedChars = "";
+let password = "";
 
-function generatePassword(length,includeLowercase,upperCase,specialChars){
+
+function generatePassword(length,includeLowercase,includeUpperCase,includeSpecialChars, includeNumbers){
   let lowerCase = "abcdefghijklmnopqrstuvwxyz";
   let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let specialChars = "!@#$%^&*_+=-";
+  let numbers = "0123456789";
 
   if(length < 0){
     return "The password length must be atleast one.";
@@ -15,13 +20,18 @@ function generatePassword(length,includeLowercase,upperCase,specialChars){
   else if(length === 0){
     return "You need to select atleast one option";
   }
-  else{
-    password += lowerCase;
-    password += upperCase;
-    password += specialChars;
+  for(i = 0; i <= length; i++){
+      allowedChars += lowerCase;
+      allowedChars += upperCase;
+      allowedChars += specialChars;
+      allowedChars += numbers;
   }
+
+   password = Math.floor(Math.random() * allowedChars);
+
+  
 }
 
- let generatedPassword = generatePassword(passwordLength, includeLowercase,includeUpperCase,includeSpecialChars);
+ let generatedPassword = generatePassword(length,includeLowercase,includeUpperCase,includeSpecialChars, includeNumbers);
 
- console.log(generatePassword);
+ console.log(password);
