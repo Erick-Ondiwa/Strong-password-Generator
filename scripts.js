8cb0f98d0e722d0830d7af3990666f0997cf32db
erick-ondiwa <erickochieng3860@gmail.com>
2024-06-13 23:57:23 +0300

    Modified

--- scripts.js
+++ scripts.js
@@ -1,4 +1,29 @@
-function generatePassword(length,includeLowercase,includeUpperCase,includeSpecialChars, includeNumbers){
+//A program that allows three users to key in their names, email addresses and password then display the details on the document.
+
+//selecting the entered name, email and password from the input fields
+let fullName = document.getElementById('fullName').value;
+let enteredEmail = document.getElementById('emailAddess');
+let enteredPassword = document.getElementById('enteredPassword').value;
+
+
+//Creating a constructor to accept the details of the three persons
+function Persons(names, emails, passwords){
+  this.names = names;
+  this.emails = emails;
+  this.passwoeds = passwords;
+}
+
+// Creating 
+const Person1 = new Persons(fullName, enteredEmail, enteredPassword);
+Person1.names;
+let personNames = document.getElementById('personNames');
+const submitButton = document.getElementById('submitDetails');
+  submitButton.addEventListener("click", () =>{
+    personNames.textContent = Person1.names + " " + Person1.emails;
+  });
+
+
+function generatePassword(length, includeLowercase, includeUpperCase, includeSpecialChars,  includeNumbers){
 
   let allowedChars = "";
   let password = "";
@@ -20,7 +45,7 @@ function generatePassword(length,includeLowercase,includeUpperCase,includeSpecia
     return "You need to select atleast one option";
   }
   for(i = 0; i < length; i++){
-   const randomIndex = Math.random() * allowedChars.length;
+   const randomIndex = Math.floor(Math.random() * allowedChars.length);
    password += allowedChars[randomIndex];
   }
 
@@ -34,6 +59,6 @@ const includeUpperCase = true;
 const includeNumbers = true;
 const includeSpecialChars = true;
 
- const password = generatePassword(passwordLength, includeLowercase, includeUpperCase,includeSpecialChars, includeNumbers);
+ const password = generatePassword(passwordLength, includeLowercase, includeUpperCase,  includeSpecialChars, includeNumbers);
 
  console.log(password);
